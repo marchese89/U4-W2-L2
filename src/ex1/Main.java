bf5dd2363f0e0e0e47d2f999bede816dab178bd2
marchese89 <marchese89@hotmail.com>
2023-12-05 09:53:00 +0100

    ex1 ok

---
+++ src/ex1/Main.java
@@ -0,0 +1,57 @@
+package ex1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserire un numero N");
+        Integer n = null;
+        while (n == null){
+            try {
+                n = Integer.parseInt(sc.nextLine());
+            }catch (NumberFormatException e){
+                System.out.println("input non corretto, riscrivere");
+            }
+        }
+        Set<String> setParole = new HashSet<>();
+        Set<String> setDuplicati = new HashSet<>();
+        Set<String> setDistinte = new HashSet<>();
+
+        for (int i = 0; i < n; i++){
+            System.out.println("dammi una parola");
+            String p = null;
+            while (p == null){
+                try {
+                    p = sc.nextLine();
+                }catch (NumberFormatException e){
+                    System.out.println("input non corretto, riscrivere");
+                }
+            }
+            if(setParole.contains(p)) {
+                setDuplicati.add(p);
+            }
+            setParole.add(p);
+        }
+
+        System.out.println("Parole duplicate");
+        stampaSet(setDuplicati);
+        //troviamo le parole distinte
+        for(String s: setParole){
+            if(!setDuplicati.contains(s)){
+                setDistinte.add(s);
+            }
+        }
+
+        System.out.println("numero parole distinte: "+setDistinte.size());
+        stampaSet(setDistinte);
+    }
+
+    public static void stampaSet(Set<String> set){
+        System.out.print("{");
+        for (String s: set){
+            System.out.print(s+" ");
+        }
+        System.out.println("}");
+    }
+}
